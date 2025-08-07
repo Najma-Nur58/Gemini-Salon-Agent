cba3165a8a46f94f6fc2b20d0b5c0b52a8e38a7c
Najma-Nur58 <najmanur558@gmail.com>
2025-08-07 12:47:41 +0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -13,10 +13,10 @@ const sendButton = chatForm.querySelector('button') as HTMLButtonElement;
 // --- App State ---
 let chat: Chat;
 const HAIR_COLOR_IMAGES = [
-    'haie4.jpg',
     'pubic/hair1.jpg',
-    'https://i.ibb.co/z4V4pFr/hair3.jpg',
-    'https://i.ibb.co/V3TjLJN/hair4.jpg',
+    'pubic/hair1.jpg',
+    'pubic/hair1.jpg',
+    'pubic/hair1.jpg',
     'https://i.ibb.co/d2xL5ZN/hair5.jpg',
 ];
 const SPECIAL_COMMANDS = {
